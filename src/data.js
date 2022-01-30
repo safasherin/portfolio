fcce03593eeebb47a353c8145596b9853b736d21
unknown <safasherin59@gmail.com>
2022-01-30 11:24:39 +0400

    updated

--- src/data.js
+++ src/data.js
@@ -10,7 +10,11 @@ export const projects = [
         img: "https://res.cloudinary.com/dv3uj18zo/image/upload/v1643100603/portfolio/blog_jqk1wu.png",
         link: "https://blogidea-frontend.herokuapp.com/",
     },
-
+    {
+        id: 3,
+        img: "https://res.cloudinary.com/dv3uj18zo/image/upload/v1643527192/portfolio/ejadahsearch_l80fwn.png",
+        link: "  https://ejadahsearch.azurewebsites.net/",
+    },
 
 
 ];
