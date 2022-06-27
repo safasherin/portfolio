302f5e181b1038514d58cacf37e0368d047da2f6
unknown <safasherin59@gmail.com>
2022-06-27 13:38:21 +0400

    images

--- src/data.js
+++ src/data.js
@@ -3,12 +3,13 @@ export const projects = [
     {
         id: 1,
         img: "https://res.cloudinary.com/dv3uj18zo/image/upload/v1643100598/portfolio/questionnaire_kuvmar.png",
-        link: "https://question-board-app-frontend.herokuapp.com/",
+        link: "https://blogidea-frontend.herokuapp.com/",
     },
     {
         id: 2,
         img: "https://res.cloudinary.com/dv3uj18zo/image/upload/v1643100603/portfolio/blog_jqk1wu.png",
-        link: "https://blogidea-frontend.herokuapp.com/",
+
+        link: "https://question-board-app-frontend.herokuapp.com/",
     },
     {
         id: 3,
